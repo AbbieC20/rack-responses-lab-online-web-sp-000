6dcca962c5ddf688f753716990b0648e72532892
remote-torrent-6747 <abbyc@sky.com>
2021-03-29 19:42:54 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,7 @@
 class Application
 
   def daily_greeting(env)
-    if Time.now.to_i
+    if Time.now.to_i.hour < 12
       resp.write "Good morning!"
     else
       resp.write "Good Afternoon!"
@@ -16,3 +16,7 @@ If it's before noon, greet the user with "Good Morning!"
 If it's after noon, greet the user with "Good Afternoon!".
 Remember that getting the time may return results in 24 hour time!
 Anytime on or after 12 is the afternoon.
+
+12
+13
+1
