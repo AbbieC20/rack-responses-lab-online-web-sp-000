1711a585b1742d4d2d0336246ad7cb8aa164d41a
remote-torrent-6747 <abbyc@sky.com>
2021-03-29 19:43:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,15 +8,3 @@ class Application
   end
 
 end
-
-
-
-
-If it's before noon, greet the user with "Good Morning!"
-If it's after noon, greet the user with "Good Afternoon!".
-Remember that getting the time may return results in 24 hour time!
-Anytime on or after 12 is the afternoon.
-
-12
-13
-1
